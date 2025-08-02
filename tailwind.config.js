0ecfef597cedcc29b6098d569ac15a708f5a6f72
Carlos PC <c68806779@gmail.com>
2025-08-02 16:45:42 -0500

    Detalles corregidos

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,17 @@ export default {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
         rubik: ['Rubik', 'sans-serif'],
-        }
-
+        },
+        colors: {
+        azul: '#0071b2',
+        gris: '#64748b',
+        azulBorder: '#b3d3e6',
+        azulfondo: '#C0DAE5',
+        blancoBody: '#f0f2f5',
+        grisInput: '#6B7280',
+        azulHover: '#005f93',
+        blancoInput: '#e0e0e0',
+      },
     },
   },
   plugins: [],
