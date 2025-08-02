6dcd1762b7053f5ccae3d7bd0a93291bb7265c86
Carlos PC <c68806779@gmail.com>
2025-08-02 15:50:52 -0500

    Agrego animaciones y fuente personalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
-        backgroundImage: {
+      backgroundImage: {
         'hero-dentista': "url('/assets/img/paciente-mujer-en-dentista.webp')",
         'footer-texture': "url('/img/footer-texture.png')",
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        rubik: ['Rubik', 'sans-serif'],
         }
-        
+
     },
   },
   plugins: [],
